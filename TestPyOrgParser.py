e2fa42e50f21ad38934e7a84eba1a8f1eccee901
biniu <dawid.binkowski93@gmail.com>
2018-10-31 09:41:53 +0100

    add task number test

--- TestPyOrgParser.py
+++ TestPyOrgParser.py
@@ -2,17 +2,21 @@
 
 import unittest
 
-import PyOrgParser
+from PyOrgParser import PyOrgParser
+
 
 class TestPyOrgParser(unittest.TestCase):
 
     test_class = None
     MOCK_FILE = 'TEST.org'
+    TASK_NUMBER = 17
 
     def setUp(self):
-        print("setUp")
         self.test_class = PyOrgParser(self.MOCK_FILE)
 
     def tearDown(self):
-        print("tearDown")
         del self.test_class
+
+    def test_get_task_number(self):
+        self.assertEqual(self.test_class.get_task_number(),
+                         self.TASK_NUMBER)
