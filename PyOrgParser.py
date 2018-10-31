5304dc297f1bb1d72c220c9364706144b26101f2
biniu <dawid.binkowski93@gmail.com>
2018-10-31 11:02:24 +0100

    add destructor and cleanUp, add method for get task name

--- PyOrgParser.py
+++ PyOrgParser.py
@@ -1,15 +1,36 @@
 
+import re
+
+
 class PyOrgParser():
+
+    TASK_STATE_R = '(TODO|DONE)'
 
     org_file_raw_list = []
 
     def __init__(self, org_file_path):
         self.__parse_org_file(org_file_path)
 
+    def __del__(self):
+        del self.org_file_raw_list[:]
+
     def get_task_number(self):
         return len(self.org_file_raw_list)
 
+    def get_task_name(self, elem):
+        out = ''
+        reg= r"%s(.*?)(\:PROPERTIES\:|DEADLINE\:|SCHEDULED\:)"\
+            % (self.TASK_STATE_R)
+        match = re.search(reg, elem)
+        if match:
+            out = match.group(2)
+            # Remove task priority
+            out = re.sub(r'\[#[A-Z]\]', '', out)
+            out = out.lstrip(' ').rstrip(' ')
+        return out
+
     def __parse_org_file(self, org_file_path):
+        del self.org_file_raw_list[:]
         with open(org_file_path) as of:
             self.org_file_raw = of.read()
 
--- TestPyOrgParser.py
+++ TestPyOrgParser.py
@@ -11,6 +11,19 @@ class TestPyOrgParser(unittest.TestCase):
     MOCK_FILE = 'TEST.org'
     TASK_NUMBER = 17
 
+    TASK_ENTRIES = [
+        ("** TODO test_task_1   SCHEDULED: <2018-10-17 śro>"
+         " DEADLINE: <2018-11-21 śro>   :PROPERTIES:   :CREATED:"
+         "  <2018-10-10 śro 12:32>   "
+         ":ID:       25b044c9-0e29-42cf-a5e1-fe4ae9cce419   :END:"),
+        ("* TODO [#A] TEST_PROJECT_2"
+         ":PROPERTIES:"
+         ":CREATED:  <2018-10-10 śro 12:31>"
+         ":ID:       ef33c4a0-34dd-4ce0-836e-fa35b25be97d"
+         ":END:"
+         ),
+    ]
+
     def setUp(self):
         self.test_class = PyOrgParser(self.MOCK_FILE)
 
@@ -20,3 +33,10 @@ class TestPyOrgParser(unittest.TestCase):
     def test_get_task_number(self):
         self.assertEqual(self.test_class.get_task_number(),
                          self.TASK_NUMBER)
+
+    def test_get_task_name(self):
+
+        self.assertEqual(self.test_class.get_task_name(
+            self.TASK_ENTRIES[0]), 'test_task_1')
+        self.assertEqual(self.test_class.get_task_name(
+            self.TASK_ENTRIES[1]), 'TEST_PROJECT_2')
