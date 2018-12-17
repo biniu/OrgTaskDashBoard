6252a1a8c4a11e20a1f919ee49f8124e235d53ea
biniu <dawid.binkowski93@gmail.com>
2018-12-17 16:17:35 +0100

    update models

--- OrgTaskDashBoard/otdb/models.py
+++ OrgTaskDashBoard/otdb/models.py
@@ -3,21 +3,33 @@ from djongo import models
 
 # Create your models here.
 
+
 class Project(models.Model):
-    name = models.CharField(max_length=255)
+    project_name = models.CharField(max_length=255)
     project_id = models.IntegerField(primary_key=True)
+    project_tags = models.ForeignKey('Tag')
     file_path = models.CharField(max_length=255)
-    # tags = ???
+
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=255)
+    tag_id = models.IntegerField(primary_key=True)
+
+
+class Status(models.Model):
+    status_name = models.CharField(max_length=255)
+    status_id = models.IntegerField(primary_key=True)
+
 
 class Task(models.Model):
     task = models.CharField(max_length=255)
     task_id = models.IntegerField(primary_key=True)
     task_org_id = models.CharField(max_length=255)  # for org files
-    level = models.IntegerField()
-    # status = ???
-    # priority = ???
-    # tags = ???
-    deadline = models.DateField()
-    created = models.DateField()
-    # parent = ???
-    # project_id
+    task_level = models.IntegerField()
+    # task_status = ???
+    # task_priority =
+    task_tags = models.ForeignKey('Tag')
+    task_deadline = models.DateField()
+    task_created = models.DateField()
+    # task_parent = ???
+    task_project_id = models.ForeignKey('Project')
